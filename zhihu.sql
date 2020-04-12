73beadc85622e22b29d660b239f435d0b45cd17b
Willbern <g.success16@gmail.com>
2020-04-12 16:14:45 +0800

    update

--- zhihu.sql
+++ zhihu.sql
@@ -5,5 +5,7 @@ CREATE TABLE IF NOT EXISTS `zhihu`(
    `user_avatar` VARCHAR(2000),
    `user_url` VARCHAR(2000),
    `user_add` VARCHAR(2000),
+   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
    PRIMARY KEY ( `user_name` )
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
